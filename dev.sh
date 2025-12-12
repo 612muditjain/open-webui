6d34a48799975794478fbc9d95a510afb7b45dea
Mudit Jain <90764676+612muditjain@users.noreply.github.com>
2025-12-12 18:41:07 +0530

    Update CORS_ALLOW_ORIGIN for development environment

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
-export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080,https://612muditjain-open-webui.vercel.app"
+export CORS_ALLOW_ORIGIN="https://63d528a59e06.ngrok-free.app;https://612muditjain-open-webui.vercel.app"
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
